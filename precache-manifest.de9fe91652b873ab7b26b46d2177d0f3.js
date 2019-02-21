192704feb5efa0e5c7c80f07c623cad3ea680198
Miroslav P <miroslav.petkevic@gmail.com>
2019-02-21 22:12:11 +0200

    Updates

---
+++ precache-manifest.de9fe91652b873ab7b26b46d2177d0f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a96cdf15ddbb7fd7b88866e959fb9bb0",
+    "url": "/tesonet-frontend/static/media/user.a96cdf15.svg"
+  },
+  {
+    "revision": "767b2789a8a359a3bfbef0ec50633140",
+    "url": "/tesonet-frontend/static/media/img.767b2789.png"
+  },
+  {
+    "revision": "8400e24c3f4fd3d9bad9",
+    "url": "/tesonet-frontend/static/js/runtime~main.8400e24c.js"
+  },
+  {
+    "revision": "3a3e5e9a89b523f16653",
+    "url": "/tesonet-frontend/static/js/main.3a3e5e9a.chunk.js"
+  },
+  {
+    "revision": "a79e27ec91c2e647dec0",
+    "url": "/tesonet-frontend/static/js/2.a79e27ec.chunk.js"
+  },
+  {
+    "revision": "3a3e5e9a89b523f16653",
+    "url": "/tesonet-frontend/static/css/main.d049e7bd.chunk.css"
+  },
+  {
+    "revision": "6332f2f1ea753b58db64c3c8a015d767",
+    "url": "/tesonet-frontend/index.html"
+  }
+];
